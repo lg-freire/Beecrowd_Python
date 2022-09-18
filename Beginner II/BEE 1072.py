6b71079f5916332f365db4aa7f6f5a62feb78e90
lg-freire <l-gabriel@outlook.com>
2022-09-18 20:05:16 -0300

    Ex 1072

---
+++ Beginner II/BEE 1072.py
@@ -0,0 +1,12 @@
+n = int(input())
+li, liw = [], []
+for i in range(0, n):
+    x = int(input())
+    if x in range(10, 21):
+        li.append(x)
+    elif x < 0:
+        x = None
+    else:
+        liw.append(x)
+print(f'{len(li)} in')
+print(f'{len(liw)} out')
