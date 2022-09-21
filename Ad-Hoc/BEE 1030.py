e29b3fab2b9b77011c5c9690e3f2df2ad4faa98f
lg-freire <l-gabriel@outlook.com>
2022-09-21 17:03:39 -0300

    Ex 1030

--- Ad-Hoc/BEE 1030.py
+++ Ad-Hoc/BEE 1030.py
@@ -1,17 +1,20 @@
-nums = []
+
 nc = int(input())
 for i in range(0, nc):
     n, k = map(int, input().split())
-    for num in range(1, n+1):
-        nums.append(num)
+    nums = list(range(1, n+1))
     print(nums)
-    while len(nums) != 1:
-        dead = 0
-        if (dead + k) < len(nums):
+    dead = 0
+    while len(nums) > 1:
+        if (dead + k) <= len(nums):
             dead += k
         else:
             dead = (dead + k) % len(nums)
-        del(nums[dead])
+        if dead != 0:
+            del nums[dead-1]
+        else:
+            del nums[dead]
+        print(nums)
         if dead == len(nums):
             dead = 0
     print(f'Case {i+1}: {nums[0]}')
