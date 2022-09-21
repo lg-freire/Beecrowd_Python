78d9a18d2852f0e4dd8bb8bb1112a627cad80613
lg-freire <l-gabriel@outlook.com>
2022-09-21 16:17:09 -0300

    Ex 1030

--- Ad-Hoc/BEE 1030.py
+++ Ad-Hoc/BEE 1030.py
@@ -5,9 +5,13 @@ for i in range(0, nc):
     for num in range(1, n+1):
         nums.append(num)
     print(nums)
-    while True:
-        if len(nums) == 1:
-            break
-        for dead in range(1, len(nums)+1, k):
-            nums.remove(dead)
+    while len(nums) != 1:
+        dead = 0
+        if (dead + k) < len(nums):
+            dead += k
+        else:
+            dead = (dead + k) % len(nums)
+        del(nums[dead])
+        if dead == len(nums):
+            dead = 0
     print(f'Case {i+1}: {nums[0]}')
