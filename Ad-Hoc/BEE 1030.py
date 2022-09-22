89593c4ee21aa56386827184673d5b515c8dd312
lg-freire <l-gabriel@outlook.com>
2022-09-22 19:49:47 -0300

    Ex 1030

--- Ad-Hoc/BEE 1030.py
+++ Ad-Hoc/BEE 1030.py
@@ -1,13 +1,19 @@
-nc = int(input())
-for i in range(0, nc):
-    n, k = map(int, input().split())
-    nums = list(range(1, n+1))
-    print(nums)
-    dead = k - 1
-    while len(nums) > 1:
-        nums.pop(dead)
-        dead += (k - 1)
-        if dead >= len(nums):
-            dead %= len(nums)
-        print(nums)
-    print(f'Case {i+1}: {nums[0]}')
+while True:
+    try:
+        nc = int(input())
+        for i in range(0, nc):
+            n, k = map(int, input().split())
+            nums = list(range(1, n+1))
+            # print(nums)
+            dead = k - 1
+            while len(nums) > 1:
+                # print(dead)
+                nums.pop(dead)
+                dead += (k - 1)
+                if dead >= len(nums):
+                    dead %= len(nums)
+                # print(nums)
+            print(f'Case {i+1}: {nums[0]}')
+        break
+    except EOFError:
+        break
