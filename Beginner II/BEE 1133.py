7740aa541483dafe48d732d227e31409faf27a64
lg-freire <l-gabriel@outlook.com>
2022-10-06 17:42:10 -0300

    Ex 1133

--- Beginner II/BEE 1133.py
+++ Beginner II/BEE 1133.py
@@ -1,8 +1,8 @@
 x = int(input())
 y = int(input())
 if x < y:
-    for i in range(x, y+1):
+    for i in range(x, y):
         if i % 5 == 2 or i % 5 == 3: print(i)
 else:
-    for i in range(y, x+1):
+    for i in range(y, x):
         if i % 5 == 2 or i % 5 == 3: print(i)
